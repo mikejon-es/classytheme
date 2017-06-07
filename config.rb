b3f0558319f393267d0978db4cb4e829965b5f99
Mike Jones <mjones@faithcatholic.com>
2017-06-07 15:58:58 -0400

    added classy theme

---
+++ config.rb
@@ -0,0 +1,13 @@
+preferred_syntx = :scss
+http_path = '/'
+css_dir = 'css'
+sass_dir = 'sass'
+images_dir = 'images'
+output_style = :expanded
+relative_assets = true
+line_comments = true
+
+require 'breakpoint'
+require 'modular-scale'
+require 'compass'
+require 'singularitygs'
